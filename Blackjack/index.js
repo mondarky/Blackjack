ec17c3865f5869c35ce08b2e4f092a6ba745bca9
Mondarky <jeeradet0jame@gmail.com>
2021-06-22 20:18:42 +0700

    Add deck building function

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,11 +1,14 @@
-let cards = [];
+let playerCards = [];
 let backJack = false;
 let isAlive = false;
 let message = "";
 let player = {
-    name : "James",
+    name : "Player",
     chips : 150
 }
+let suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let deck = new Array();
 
 
 let messageEl = document.getElementById("message-el");
@@ -14,54 +17,52 @@ let cardEl = document.getElementById("card-el");
 let buttonEl = document.getElementById("button-el");
 let playerEl = document.getElementById("player-el");
 playerEl.textContent = player.name + " : " + player.chips;
+
 function startGame(){
-    displayPlayButton()
+    displayPlayButton();
+    createDeck();
     isAlive = true;
     backJack = false;
-    cards = [];
-    cards.push(getRandomCard());
-    cards.push(getRandomCard());
+    playerCards = [];
+    playerCards.push(drawCard());
+    playerCards.push(drawCard());
     renderGame();
 }
 function renderGame(){
     let sumCard = 0;
     let stringCard = "";
-    for(c in cards){
-        sumCard += cards[c];
-        stringCard += String(cards[c]) + " "
+    for(c in playerCards){
+        sumCard += playerCards[c].Weight;
+        stringCard += String(playerCards[c].Value) + " "
     }
     sumEl.textContent = "Sum: " + String(sumCard);
-    cardEl.textContent = "Cards: " + stringCard;
+    cardEl.textContent = "playerCards: " + stringCard;
+    gameCaculation(sumCard);
     
-    if (sumCard < 21){
-        message = "Hit?";
-        isAlive = true;
-    }else if (sumCard === 21){
-        cardBackJack();
-    }else {
-        cardBust()
-    }
     messageEl.textContent = message;
     
 }
+function gameCaculation(sum){
+    if (sum < 21){
+        message = "Hit?";
+        isAlive = true;
+    }else if (sum === 21){
+        cardBackJack();
+    }else {
+        cardBust();
+    }
+}
 function hitCard(){
-    cards.push(getRandomCard());
+    playerCards.push(drawCard());
     renderGame();
 }
 function stayCard(){
-
+    displayNewGameButton();
 }
 
-function getRandomCard(){
-    let randomNumber = Math.floor(Math.random()*13) + 1
-    if(randomNumber > 10){
-        return 10;
-    } else if (randomNumber === 1){
-        return 11;
-    } else {
-        return randomNumber;
-    }
-    
+function drawCard(){
+    let draw = deck.pop();
+    return draw;
 }
 function cardBackJack(){
     message = "Blackjack!";
@@ -74,6 +75,35 @@ function cardBust(){
     isAlive = false;
     displayNewGameButton()
 }
+
+function createDeck(){
+    deck = new Array();
+    for(var x = 0; x < suits.length; x++){
+        for (var i = 0 ; i < values.length; i++){
+            var weight = parseInt(values[i]);
+            if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
+                weight = 10;
+            if (values[i] == "A")
+                weight = 11;
+            var card = { Value: values[i], Suit: suits[x], Weight: weight };
+            deck.push(card);
+        }
+    }
+    shuffle();
+}
+function shuffle(){
+        // for 1000 turns
+        // switch the values of two random playerCards
+        for (var i = 0; i < 1000; i++){
+            var location1 = Math.floor((Math.random() * deck.length));
+            var location2 = Math.floor((Math.random() * deck.length));
+            var tmp = deck[location1];
+
+            deck[location1] = deck[location2];
+            deck[location2] = tmp;
+        }
+    }
+
 function displayStartButton(){
     document.getElementById('button-el').innerHTML = `
     <button class='start-button' onclick="startGame()" >START GAME</button>
